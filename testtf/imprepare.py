98e7e4b179362ff08763ae8c0cf8c4aa63637b93
slehaff <samir@lehaff.dk>
2019-07-30 13:27:56 +0200

    sa

--- testtf/imprepare.py
+++ testtf/imprepare.py
@@ -1,6 +1,5 @@
 from PIL import Image
 from enhance import *
-import numpy as np
 
 
 indexarray= [[103,0],[271,10],[410,20],[103,137],[271,137],[410,137],[103,412], [271,271], [410,281]]
@@ -13,9 +12,9 @@ def imslice(imgpath, destfolder, offset, istart, iend, jstart, jend):
     out_width = 64
     for i in range(istart, iend):
         for j in range(jstart, jend):
-            myslice = img.crop((i*out_width, j*out_height, i *
+            slice = img.crop((i*out_width, j*out_height, i *
                               out_width+64, j*out_height+64))
-            myslice.save(destfolder + str(n) + '.png')
+            slice.save(destfolder + str(n) + '.png')
             n += 1
     return
 
@@ -23,40 +22,33 @@ def im2slice(imgpath, destfolder, offset, winindex):
     img = Image.open(imgpath)
     n = offset
     for i in range(len(winindex)):
-        myslice = img.crop((winindex[i][0], winindex[i][1], winindex[i][0]+170, winindex[i][1]+170))
-        myslice.save(destfolder + str(n) + '.png')
-        n += 1
+        slice = img.crop((winindex[i][0], winindex[i][1], winindex[i][0]+170, winindex[i][1]+170))
+        slice.save(destfolder + str(n) + '.png')
+        if i == 6:
+            print(winindex[i][0], winindex[i][1], winindex[i][0]+170, winindex[i][1]+170)
+        n+=1
 
     return
 
-def npy2slice(imgpath, destfolder, offset, winindex):
-    n =  offset
-    img = np.load(imgpath)
-    for i in range(len(winindex)):
-        myslice = img[winindex[i][1]:winindex[i][1]+170, winindex[i][0]:winindex[i][0]+170]
-        np.save(destfolder + str(n) + '.npy', myslice, allow_pickle=False)
-        n += 1
 
 
-
-
-# def folderLoad(offset, folder):
-#     imgpath1 = './testtf/data/' + folder + '/image3.png'
-#     destfolder1 = 'fringeA/'
-#     imgpath2 = './testtf/data/' + folder + '/gray.png'
-#     destfolder2 = 'gray/'
-#     imgpath3 = './testtf/data/' + folder + '/b1nom.png'
-#     destfolder3 = 'nom/'
-#     imgpath4 = './testtf/data/' + folder + '/b1denom.png'
-#     destfolder4 = 'denom/'
-#     istart = 2
-#     iend = 9
-#     jstart = 1
-#     jend = 6
-#     imslice(imgpath1, destfolder1, offset, istart, iend, jstart, jend)
-#     imslice(imgpath2, destfolder2, offset, istart, iend, jstart, jend)
-#     imslice(imgpath3, destfolder3, offset, istart, iend, jstart, jend)
-#     imslice(imgpath4, destfolder4, offset, istart, iend, jstart, jend)
+def folderLoad(offset, folder):
+    imgpath1 = './testtf/data/' + folder + '/image3.png'
+    destfolder1 = 'fringeA/'
+    imgpath2 = './testtf/data/' + folder + '/gray.png'
+    destfolder2 = 'gray/'
+    imgpath3 = './testtf/data/' + folder + '/b1nom.png'
+    destfolder3 = 'nom/'
+    imgpath4 = './testtf/data/' + folder + '/b1denom.png'
+    destfolder4 = 'denom/'
+    istart = 2
+    iend = 9
+    jstart = 1
+    jend = 6
+    imslice(imgpath1, destfolder1, offset, istart, iend, jstart, jend)
+    imslice(imgpath2, destfolder2, offset, istart, iend, jstart, jend)
+    imslice(imgpath3, destfolder3, offset, istart, iend, jstart, jend)
+    imslice(imgpath4, destfolder4, offset, istart, iend, jstart, jend)
 
 
 def folder2load(offset, folder, winindex):
@@ -64,14 +56,14 @@ def folder2load(offset, folder, winindex):
     destfolder1 = 'fringeA/'
     imgpath2 = './testtf/data/' + folder + '/gray.png'
     destfolder2 = 'gray/'
-    imgpath3 = './testtf/data/' + folder + '/1nom.npy'
+    imgpath3 = './testtf/data/' + folder + '/b1nom.png'
     destfolder3 = 'nom/'
-    imgpath4 = './testtf/data/' + folder + '/1denom.npy'
+    imgpath4 = './testtf/data/' + folder + '/b1denom.png'
     destfolder4 = 'denom/'
     im2slice(imgpath1, destfolder1, offset, winindex)
     im2slice(imgpath2, destfolder2, offset, winindex)
-    npy2slice(imgpath3, destfolder3, offset, winindex)
-    npy2slice(imgpath4, destfolder4, offset, winindex)
+    im2slice(imgpath3, destfolder3, offset, winindex)
+    im2slice(imgpath4, destfolder4, offset, winindex)
 
 
 
@@ -81,23 +73,12 @@ def makegray(folder):
     img.save('./testtf/data/' + folder + '/gray.png')
 
 
-# makegray('train/1scan_im_folder')
-# makegray('train/2scan_im_folder')
-# makegray('train/3scan_im_folder')
-# makegray('train/4scan_im_folder')
-# makegray('train/5scan_im_folder')
-# makegray('train/6scan_im_folder')
-# makegray('train/7scan_im_folder')
-# makegray('train/8scan_im_folder')
-# makegray('train/9scan_im_folder')
-# makegray('train/10scan_im_folder')
-# makegray('train/11scan_im_folder')
-# makegray('train/12scan_im_folder')
-# makegray('train/13scan_im_folder')
-# makegray('train/14scan_im_folder')
-# makegray('train/15scan_im_folder')
-# makegray('train/16scan_im_folder')
-# makegray('train/17scan_im_folder')
+# makegray('teeth1')
+# makegray('teeth2')
+# makegray('teeth3')
+# makegray('teeth4')
+# makegray('teeth5')
+# makegray('teeth6')
 
 
 # folderLoad(0, 'teeth1')
@@ -108,69 +89,51 @@ def makegray(folder):
 # folderLoad(175, 'teeth6')
 
 def make(foldername, offset):
-    # brighter(foldername)
-    # equalizeImg(foldername)
-    # makegray(foldername)
+    brighter(foldername)
+    equalizeImg(foldername)
+    makegray(foldername)
     folder2load(offset, foldername, indexarray)
 
 
-# make('v21', 0)
-# make('v22', 9)
-# make('v23', 9*2)
-# make('v24', 9*3)
-# make('v25', 9*4)
-# make('v26', 9*5)
-# make('v27', 9*6)
-# make('v28', 9*7)
-# make('v29', 9*8)
-# make('v211', 9*9)
-# make('v212', 9*10)
-# make('v213', 9*11)
-# make('v214', 9*12)
-# make('v215', 9*13)
-# make('v215', 9*14)
-# make('train/1scan_im_folder', 9*15)
-# make('train/2scan_im_folder', 9*16)
-# make('train/3scan_im_folder', 9*17)
-# make('train/4scan_im_folder', 9*18)
-# make('train/5scan_im_folder', 9*19)
-# make('train/6scan_im_folder', 9*20)
-# make('train/7scan_im_folder', 9*21)
-# make('train/8scan_im_folder', 9*22)
-# make('train/9scan_im_folder', 9*23)
-# make('train/10scan_im_folder', 9*24)
-# make('train/11scan_im_folder', 9*25)
-# make('train/12scan_im_folder', 9*26)
-# make('train/13scan_im_folder', 9*27)
-# make('train/14scan_im_folder', 9*28)
-# make('train/15scan_im_folder', 9*29)
-# make('train/16scan_im_folder', 9*30)
-# make('train/17scan_im_folder', 9*31)
-# make('train/18scan_im_folder', 9*32)
-# make('train/19scan_im_folder', 9*33)
-# make('train/20scan_im_folder', 9*34)
-# make('train/21scan_im_folder', 9*35)
-# make('train/22scan_im_folder', 9*36)
-# make('train/23scan_im_folder', 9*37)
-# make('train/24scan_im_folder', 9*38)
-
-make('train/1scan_im_folder', 9*0)
-make('train/2scan_im_folder', 9*1)
-make('train/3scan_im_folder', 9*2)
-make('train/4scan_im_folder', 9*3)
-make('train/5scan_im_folder', 9*4)
-make('train/6scan_im_folder', 9*5)
-make('train/7scan_im_folder', 9*6)
-make('train/8scan_im_folder', 9*7)
-make('train/9scan_im_folder', 9*8)
-make('train/10scan_im_folder', 9*9)
-make('train/11scan_im_folder', 9*10)
-make('train/12scan_im_folder', 9*11)
-make('train/13scan_im_folder', 9*12)
-make('train/14scan_im_folder', 9*13)
-make('train/15scan_im_folder', 9*14)
-make('train/16scan_im_folder', 9*15)
-make('train/17scan_im_folder', 9*16)
+make('v21', 0)
+make('v22', 9)
+make('v23', 9*2)
+make('v24', 9*3)
+make('v25', 9*4)
+make('v26', 9*5)
+make('v27', 9*6)
+make('v28', 9*7)
+make('v29', 9*8)
+make('v211', 9*9)
+make('v212', 9*10)
+make('v213', 9*11)
+make('v214', 9*12)
+make('v215', 9*13)
+make('v215', 9*14)
+make('train/1scan_im_folder', 9*15)
+make('train/2scan_im_folder', 9*16)
+make('train/3scan_im_folder', 9*17)
+make('train/4scan_im_folder', 9*18)
+make('train/5scan_im_folder', 9*19)
+make('train/6scan_im_folder', 9*20)
+make('train/7scan_im_folder', 9*21)
+make('train/8scan_im_folder', 9*22)
+make('train/9scan_im_folder', 9*23)
+make('train/10scan_im_folder', 9*24)
+make('train/11scan_im_folder', 9*25)
+make('train/12scan_im_folder', 9*26)
+make('train/13scan_im_folder', 9*27)
+make('train/14scan_im_folder', 9*28)
+make('train/15scan_im_folder', 9*29)
+make('train/16scan_im_folder', 9*30)
+make('train/17scan_im_folder', 9*31)
+make('train/18scan_im_folder', 9*32)
+make('train/19scan_im_folder', 9*33)
+make('train/20scan_im_folder', 9*34)
+make('train/21scan_im_folder', 9*35)
+make('train/22scan_im_folder', 9*36)
+make('train/23scan_im_folder', 9*37)
+make('train/24scan_im_folder', 9*38)
 
 
     
