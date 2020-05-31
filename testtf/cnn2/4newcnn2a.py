b466949628b019befb5f9aa3f239be1b6ce90c51
slehaff <samir@lehaff.dk>
2020-05-31 11:11:33 +0200

    sa

--- testtf/cnn2/4newcnn2a.py
+++ testtf/cnn2/4newcnn2a.py
@@ -18,7 +18,7 @@ from keras.utils import plot_model
 
 from nnwrap import *
 
-number_of_epochs = 500
+number_of_epochs = 100
 IMAGECOUNT = 150
 
 
@@ -340,6 +340,6 @@ for i in range(0, 150, 1):
 
 save4nnwrap()
 save4wrap()
-model.save('models/cnn2a-bmodel-shd-4npy-150-500.h5')
+# model.save('models/cnn2a-bmodel-shd-4npy-150-500.h5')
 cv2.imwrite('validate/'+'cnn2a-shd-4npy-150-500-0.png',
             (1.0*combotot).astype(np.uint8))
